3b0e24bc86df8c24b7f773ef6e8a940b42a61f09
VasilyMa <vasilymigulin@gmail.com>
2022-08-23 10:49:54 +0300

    no message

--- Assets/Scripts/SearchComponents/BaseTagMB.cs
+++ Assets/Scripts/SearchComponents/BaseTagMB.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseTagMB : MonoBehaviour
 {
     public bool isFriendly; // to do ay del this after create baseSpawnSystems
-    public GetMonster GetMonster;
+    public List<MonsterStorage> monster = new List<MonsterStorage>();
 }
